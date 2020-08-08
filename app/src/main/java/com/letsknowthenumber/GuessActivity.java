ccf02966da90521e8cd74026d4a922cec1e91003
ceren <cemilecerenerdem@gmail.com>
2020-08-08 23:40:30 +0300

    Action bar kaldırıldı.

--- app/src/main/java/com/letsknowthenumber/GuessActivity.java
+++ app/src/main/java/com/letsknowthenumber/GuessActivity.java
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +24,10 @@ public class GuessActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.activity_guess);
 
         txtRemaining = findViewById(R.id.txw_remainingright);
--- app/src/main/java/com/letsknowthenumber/ResultActivity.java
+++ app/src/main/java/com/letsknowthenumber/ResultActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -14,6 +16,10 @@ public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_result);
 
         txtResult = findViewById(R.id.txt_result);
